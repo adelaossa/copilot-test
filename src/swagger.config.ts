06e959d2941f4d4053a1f2af4770e90cf1e4e766
Andres DelaOssa <andres@comercializadora-s3.com>
2025-02-24 15:51:32 -0500

    added oauth login windows from swagger and exported realm configuration for keycloak

--- src/swagger.config.ts
+++ src/swagger.config.ts
@@ -8,16 +8,36 @@ export function setupSwagger(app: INestApplication): void {
     .setVersion('1.0')
     .addBearerAuth(
       {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        description: 'Enter your Bearer token',
-        in: 'header',
+        type: 'oauth2',
+        flows: {
+          implicit: {
+            authorizationUrl: `${process.env.KEYCLOAK_AUTH_SERVER_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/auth`,
+            tokenUrl: `${process.env.KEYCLOAK_AUTH_SERVER_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`,
+            scopes: {
+              'profile': 'Profile information',
+              'email': 'Email information',
+              'roles': 'User roles',
+            }
+          },
+        },
       },
       'access-token',
     )
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
+  
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      oauth2RedirectUrl: 'http://localhost:3000/api-docs/oauth2-redirect.html',
+      initOAuth: {
+        clientId: process.env.KEYCLOAK_CLIENT_ID,
+        realm: process.env.KEYCLOAK_REALM,
+        appName: 'NestJS API',
+        usePkceWithAuthorizationCodeGrant: true
+      },
+      persistAuthorization: true
+    },
+    customSiteTitle: 'API Documentation',
+  });
 }
